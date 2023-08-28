173c0111b6e87dccff6888438a6ad2dc771b763f
Conner Smith <connerpsmith@gmail.com>
2023-08-28 14:24:29 -0500

    initializing code

---
+++ SlackBskyUnfurl/Controllers/SlackController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SlackBskyUnfurl.Controllers; 
+
+[Route("api/slack")]
+[ApiController]
+public class SlackController : Controller {
+    private readonly ILogger<SlackController> _logger;
+
+    public SlackController(ILogger<SlackController> logger) {
+        this._logger = logger;
+    }
+
+    [HttpGet("test")]
+    public ActionResult Test() {
+        return this.Ok("test");
+    }
+}
